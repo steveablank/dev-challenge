76272237e18ff7b9adc3f242438f46f07fb82edb
Steve Blank <steveablank@gmail.com>
2013-10-06 23:35:27 -0400

    10 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,111 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name:Steve Blank
+
+# Exercise 2.1
+zipcode = 02132
+print zipcode
+zipcode2 = '02132'
+print zipcode2
+zipcode3 = 01
+print zipcode3
+zipcode4 = 010
+print zipcode4
+zipcode5 = 0100
+print zipcode5
+zipcode6 = 01000
+print zipcode6
+
+#Leading zero causes number to be octal.  
+
+
+# Exercise 2.2
+
+#>>> 5
+#5
+#>>> x=5
+#>>> x + 1
+#6
+#>>> 
+
+5
+x = 5
+x + 1
+
+print 5
+print 'x = 5'
+print x
+print x + 1
+print (x + 1)
+print ('x + 1')
+
+
+# Exercise 2.3
+# Assignment Statements
+width=17
+height=12.0
+delimiter='.'
+
+# Print Statements
+print width/2			#8
+print type((width/2))	#<type 'int'>
+print width/2.0  		#8.5
+print type(width/2.0)	#<type 'float'>
+print height/3			#4.0
+print type(height/3)	#<type 'float'>
+print 1+2*5				#11	
+print type(1+2*5)		#<type 'int'>
+print delimiter*5		#'.....'
+print type(delimiter*5)	#<type 'str'>
+
+
+# Exercise 2.4
+#1) Volume (r) = 4/3 pi r^3
+
+pi = 3.141592653589793
+r = 5.0
+x = (4.0/3.0)
+#= (x)*pi*r^3  PEMDAS
+
+print x * pi * (r**3)	#523.598775598
+
+#2) 
+book = 24.95
+discount = .40 * book
+dbook = book - discount
+ship1st = 3.0
+shipall = 0.75
+n = 60
+totalcost = ship1st + ((n-1)*shipall) + (n*dbook)
+print totalcost			#945.45
+
+#3)
+leavehour = 6
+leavemin = 52
+leavesec = 0
+
+easymin = 8
+easysec = 15
+tempomin = 7
+temposec = 12
+easymiles = 2
+tempomiles = 3
+
+tempominutes = tempomiles * tempomin
+temposeconds = tempomiles * temposec
+easyminutes = easymiles * easymin
+easyseconds = easymiles * easysec
+
+totalminutes = tempominutes + easyminutes	#37
+totalseconds = temposeconds + easyseconds	#66
+extramin = (totalminutes/60)				#0
+extrasec = (totalseconds/60)				#1
+adjminutes = totalminutes + extrasec		#38
+adjseconds = totalseconds 					#66
+arrivesec = adjseconds - 60
+print arrivesec
+arrivemin = 120-(leavemin + adjminutes)
+print arrivemin
+arrivehour = leavehour + 1
+print arrivehour
+print "You arrive home at %s:%s:" % (arrivehour, arrivemin) +"0"+"%s AM" %(arrivesec)
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,83 @@
 # Exercises for chapter 3:
 
-# Name:
+# Name:Steve Blank
+
+#3.1 
+#def print_lyrics():								original
+#	print "I'm a lumberjack, and I'm okay."			original
+#	print "I sleep all night and I work all day"	original
+#
+#def repeat_lyrics():								original
+#	print_lyrics()									original
+#	print_lyrics()									original
+#
+#repeat_lyrics()									original
+
+#repeat_lyrics()									new
+
+#def print_lyrics():								new
+#	print "I'm a lumberjack, and I'm okay."			new
+#	print "I sleep all night and I work all day"	new
+
+#def repeat_lyrics():								new
+#	print_lyrics()									new
+#	print_lyrics()									new
+	
+#This is the error received when running:			answer
+#NameError: name 'repeat_lyrics' is not defined		answer
+
+#3.2
+def repeat_lyrics():
+	print_lyrics()
+	print_lyrics()	
+
+def print_lyrics():
+	print "I'm a lumberjack, and I'm okay."
+	print "I sleep all night and I work all day"
+
+repeat_lyrics()
+
+#The program still seems to run okay when I switch the location of both definitions.
+#I'm a lumberjack, and I'm okay.
+#I sleep all night and I work all day
+#I'm a lumberjack, and I'm okay.
+#I sleep all night and I work all day
+#[Finished in 0.1s]
+
+
+#3.3
+#print len('allen')
+
+def right_justify(s, location):
+	print (' '*(location-len(s))+s)
+location = 70	
+right_justify('allen', location)
+
+#3.4.1
+
+def do_twice(f):
+	f()
+	f()
+
+def print_spam():
+	print 'spam'
+
+do_twice(print_spam)
+
+#3.4.2
+
+def do_four(f):
+	do_twice(f)
+	do_twice(f)
+
+do_four(print_spam)
+
+#3.4.3
+def print_twice(n):
+	print n
+	print n
+n="a string as a parameter printed twice"
+print_twice(n)
+
+#3.4.4
+do_twice(print_spam)
